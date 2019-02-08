022efd6fdd9d135c74e2339235ab8ce3d0831c8a
Alexey Solyanik <solyanik.a@medindex.ru>
2019-02-08 13:35:35 +0300

    Add getPrimes (use generators)

--- src/functions/get-primes.function.ts
+++ src/functions/get-primes.function.ts
@@ -1,8 +1,15 @@
+/**
+ * Получить массив простых чисел до заданного числа
+ *
+ * @export
+ * @param {number} n заданное число
+ * @returns {number[]}
+ */
 export function getPrimes(n: number): number[] {
     const result = [];
 
     Array.from(Array(n + 2).keys()).forEach(i => {
-        if (isPrime(i)) {
+        if (isPrime2(i)) {
             result.push(i);
         }
     });
@@ -10,30 +17,67 @@ export function getPrimes(n: number): number[] {
     return result;
 }
 
-function isPrime(n: number): boolean {
-    if (n < 2) {
-        // option
+/**
+ * Получить массив простых чисел в количестве n штук
+ * (используются js-генераторы)
+ *
+ * @export
+ * @param {number} count количество чисел
+ * @returns {number[]}
+ */
+export function getPrimes2(count: number): number[] {
+    return [...take(count, getPrimesSequence())];
+}
+
+/**
+ * Проверка числа на простое число
+ *
+ * @param {number} n проверяемое число
+ * @returns {boolean}
+ */
+function isPrime2(n: number): boolean {
+    if (n === 0 || n === 1) {
         return false;
     }
-    if (n <= 3) {
-        return true;
-    } else if (n % 2 === 0 || n % 3 === 0) {
-        return false;
-    }
 
-    // далее проверяем каждое нечетное число,
-    // которое не делится на 3,
-    // пока оно не станет больше квадратного корня из числа.
-
-    let i = 5;
-
-    while (i * i <= n) {
-        if (n % i === 0 || n % (i + 2) === 0) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
             return false;
         }
-
-        i += 6;
     }
 
     return true;
+}
+
+/**
+ * Генератор.
+ * Последовательность простых чисел
+ */
+function* getPrimesSequence() {
+    let n = 2;
+
+    while (true) {
+        if (isPrime2(n)) {
+            yield n;
+        }
+
+        n++;
+    }
+}
+
+/**
+ * Взять из последовательности n-чисел
+ * @param length количество чисел
+ * @param iterable последовательность
+ */
+function* take(length: number, iterable: any) {
+    for (const x of iterable) {
+        if (length <= 0) {
+            return;
+        }
+
+        length--;
+
+        yield x;
+    }
 }
--- tests/get-primes.spec.ts
+++ tests/get-primes.spec.ts
@@ -1,4 +1,4 @@
-import { getPrimes } from '../src/functions/get-primes.function';
+import { getPrimes, getPrimes2 } from '../src/functions/get-primes.function';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -28,5 +28,33 @@ describe('Test getPrimes()', () => {
         expect(primes.length).to.equal(
         [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103]
         .length);
+    });
 });
+
+describe('Test getPrimes2()', () => {
+
+    it('Get primes2 count 4', () => {
+        const primes = getPrimes2(4);
+        expect(primes.length).to.equal([2, 3, 5, 7].length);
+    });
+
+    it('Get primes2 count 6', () => {
+        const primes = getPrimes2(6);
+        expect(primes.length).to.equal([2, 3, 5, 7, 11, 13].length);
+    });
+
+    it('Get primes2 count 9',
+        () => {
+            const primes = getPrimes2(9);
+            expect(primes.length).to.equal(
+            [2, 3, 5, 7, 11, 13, 17, 19, 23].length);
+    });
+
+    it('Get primes2 count 27',
+    () => {
+        const primes = getPrimes2(27);
+        expect(primes.length).to.equal(
+        [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103]
+        .length);
+    });
 });
